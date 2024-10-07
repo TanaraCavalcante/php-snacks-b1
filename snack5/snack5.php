7bfc4f28ab23cbbbcabcc908f981010aa4e5156b
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-10-07 13:28:54 -0400

    struttura e colegamento

--- snack5/snack5.php
+++ snack5/snack5.php
@@ -1,9 +1,5 @@
 <!-- Creare una funzione che controlli se una parola (o una frase) sia palindroma, senza usare funzioni built-in, e ritorni true se lo e', false altrimenti. Testarla attraverso una chiamata GET via form. -->
 
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,13 +10,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <main>
+    <header class="mb-2 py-3 text-center">
+        <h2>Palindrome!</h2>
+    </header>
+    <main class="container">
         <section>
-            <form action="snack5.php" method="get">
-                <div class="mb-3">
-                    <label for="word" class="form-label">Maximum vote</label>
-                    <input type="number" name="voto" class="form-control" id="word" aria-describedby="voto-help">
-                    <div id="voto-help" class="form-text">Filter students by entering the maximum vote desired.</div>
+            <form action="response.php" method="get">
+                <div class="mb-2">
+                    <input type="text" name="palindrome" class="form-control" aria-describedby="word-help">
+                    <div id="word-help" class="form-text">Scrivi la tua parola per sapere se è palindroma.</div>
                 </div>
                 <button type="submit" class="btn btn-primary">submit</button>
             </form>
